4ce1dba4bbf83bd47e1befd8a5b2237a7f6e79b2
zmmohibul <18-36427-1@student.aiub.edu>
2023-07-28 01:56:20 +0600

    Auction Service

---
+++ src/AuctionService/Dtos/UpdateAuctionDto.cs
@@ -0,0 +1,10 @@
+namespace AuctionService.Dtos;
+
+public class UpdateAuctionDto
+{
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int? Year { get; set; }
+    public string Color { get; set; }
+    public int? Mileage { get; set; }
+}
---
+++ src/AuctionService/Program.cs
@@ -0,0 +1,42 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AuctionDbContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
+{
+    DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+    throw;
+}
+
+app.Run();
